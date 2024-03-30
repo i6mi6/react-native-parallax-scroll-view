5b9f3115e6604c3378cdef5010ec78b5062a2ade
AP Fritts <2000021+apfritts@users.noreply.github.com>
2024-03-30 10:40:32 -0700

    Conversion to TypeScript

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-const StyleSheet = require('react-native').StyleSheet;
+import {StyleSheet} from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -31,4 +31,4 @@ const styles = StyleSheet.create({
   }
 });
 
-module.exports = styles;
+export default styles;
